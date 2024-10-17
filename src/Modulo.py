fff7be6384c08121db8e88cbbb298a116ef0b4b9
javilopeeez <javier.lopez.novas@udc.es>
2024-10-17 11:52:35 +0200

    GUI Implementation - first steps

--- src/Modulo.py
+++ src/Modulo.py
@@ -1,22 +1,17 @@
 import pandas as pd
 import sqlite3
 import tkinter as tk
+from tkinter import filedialog, messagebox
+from tkinter import ttk
 import os
 
-root = tk.Tk() #Crea la ventana principal
-root.title("Data Importation Interface") #Le doy título a la ventana
-
-load_button = tk.Button(root, text="Load File", command=lambda: load_file())
-load_button.pack(pady=20) #pack() coloca el boton en la ventana con un espacio vertical de 20 pixeles alrededor
-
-file_label = tk.Label(root, text="File's Route: None")
-file_label.pack(pady=10) #pack() coloca la etiqueta en la ventana con un espacio vertical de 10 pixeles 
-
 class DataImport():
-    def __init__(self):
-        self._data = None
-        self._file = None
+    
+    def __init__(self, file):
 
+        self._file = file
+        self._data = None
+    
     def read_csv(self):
         try:
             self._data = pd.read_csv(self._file)
@@ -85,6 +80,7 @@ class DataImport():
 
 if __name__ == "__main__":
     DataImport().read_file()
+
             
             
 
