48da8982d3c58708c2a1969ffbf9d91187642d5a
DCouce <diego.couceiro@udc.es>
2024-10-18 18:26:12 +0200

    GUI - Implementation (Errors)

--- src/GUI-development.py
+++ src/GUI-development.py
@@ -63,6 +63,10 @@ class GUI():
                     self.display_data(data)
                 else:
                     messagebox.showerror("Error", "No data to display. Please check the file.")
+            except FileNotFoundError:
+                messagebox.showerror("Error", "File not found.")
+            except ValueError:
+                messagebox.showerror("Error", "Invalid data.")
             except Exception as e:
                 messagebox.showerror("Error", f"An error occurred while loading the file: {str(e)}")
 
