cd1e4faa0cdfef39b4b3fac0d2227ae4c758530a
Rodrigo <rodrigo.romay@udc.es>
2024-11-29 18:42:42 +0100

    feat:Corrección test predicciones #35

--- src/tests/test_predictions.py
+++ src/tests/test_predictions.py
@@ -1,16 +1,18 @@
 import pytest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 from predictions import Predictions
 from tkinter import Tk
 import customtkinter as ctk
 
+
 @pytest.fixture
 def setup_predictions():
-    root = Tk()  # Crear un root de tkinter para usar en la ventana de predicciones
-    ctk.set_appearance_mode("light")
-    formula = {"formula": "y = [2.0] * ['x1'] + 5.0"}  # Ejemplo de fórmula simple
-    result_prediction_label_mock = Mock()
-    return Predictions(formula, root, result_prediction_label_mock)
+    with patch("tkinter.Tk"):
+        root = Tk()  # Crear un root de tkinter para usar en la ventana de predicciones
+        ctk.set_appearance_mode("light")
+        formula = {"formula": "y = [2.0] * ['x1'] + 5.0"}  # Ejemplo de fórmula simple
+        result_prediction_label_mock = Mock()
+        return Predictions(formula, root, result_prediction_label_mock)
 
 def test_calculate_prediction_valid_input(setup_predictions):
     """Test to ensure a valid prediction is calculated when inputs are correct."""
